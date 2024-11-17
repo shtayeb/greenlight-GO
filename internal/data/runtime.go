d2827741b8d1eb7fdacc38b8b8101374da06b600
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-11-17 13:17:01 +0430

    custom json decoding

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -16,4 +20,31 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Split the string to isolate the part containing the number.
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	// Sanity check the parts of the string to make sure it was in the expected format
+	// If it is'nt, we return the ErrInvalidRuntimeFormat error again
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Otherwise, parse the string containing the number into an int32 again
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Assign it to a receiver, use the * operator to deference the receiver (which is a pointer to a Runtime)
+	*r = Runtime(i)
+
+	return nil
 }
