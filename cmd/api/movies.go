e51d1711c1a0f75a3197ff4bb38d5ddc7ce48cf7
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-11-17 11:55:33 +0430

    WIP

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -9,12 +10,28 @@ import (
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// When calling Decode() you must pass a non-nil pointer as the target decode destination. if not it will return json.InvalidUnmarshalError
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
-	if err != nil || id < 1 {
+	if err != nil {
 		app.notFoundResponse(w, r)
 		return
 	}
@@ -22,7 +39,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	movie := data.Movie{
 		ID:        id,
 		CreatedAt: time.Now(),
-		Title:     "Casablance",
+		Title:     "Casablanca",
 		Runtime:   102,
 		Genres:    []string{"drama", "romance", "war"},
 		Version:   1,
