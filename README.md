83e5450d7193a063e9f4268bb672770ea05e328e
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-11-18 12:01:39 +0430

    partials updates

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
+## Clone the repo
+```shell
+git clone https://github.com/shtayeb/greenlight-GO
+```
+
+## Postgres Database
+
+```shell
+psql -U postgres
+```
+Create a postgres database
+```sql
+CREATE DATABASE go_user_db OWNER go_user;
+```
+
 ## Set DB DSN env variable
 ```shell
 GREENLIGHT_DB_DSN="postgres://go_user:go_1234@localhost/snippetbox"
 ```
 
 ## Run migrations
-use GO migrate tool
+install go migrate tool
+```shell
+curl -L https://github.com/golang-migrate/migrate/releases/download/v4.14.1/migrate.linux-amd64.tar.gz | tar xvz
+
+mv migrate.linux-amd64 $GOPATH/bin/migrate
+```
+
+use GO migrate tool to apply migrations
 ```shell
 migrate -path=./migrations -database=$GREENLIGHT_DB_DSN up
 ```
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -98,10 +98,10 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 
 	// declare the input struct to hold the expected data from the client
 	var input struct {
-		Title   string       `json:"title"`
-		Year    int32        `json:"year"`
-		Runtime data.Runtime `json:"runtime"`
-		Genres  []string     `json:"genres"`
+		Title   *string       `json:"title"`
+		Year    *int32        `json:"year"`
+		Runtime *data.Runtime `json:"runtime"`
+		Genres  []string      `json:"genres"`
 	}
 
 	err = app.readJSON(w, r, &input)
@@ -111,10 +111,26 @@ func (app *application) updateMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	// Copy the values from the request body to the appropriate fields of the movie// record.
-	movie.Title = input.Title
-	movie.Year = input.Year
-	movie.Runtime = input.Runtime
-	movie.Genres = input.Genres
+	// If the input.Title value is nil then we know that no corresponding "title" key/
+	// value pair was provided in the JSON request body. So we move on and leave the
+	// movie record unchanged. Otherwise, we update the movie record with the new title
+	// value. Importantly, because input.Title is a now a pointer to a string, we need
+	// to dereference the pointer using the * operator to get the underlying value before assigning it to our movie record.
+	if input.Title != nil {
+		movie.Title = *input.Title
+	}
+
+	if input.Year != nil {
+		movie.Year = *input.Year
+	}
+
+	if input.Runtime != nil {
+		movie.Runtime = *input.Runtime
+	}
+
+	if input.Genres != nil {
+		movie.Genres = input.Genres
+	}
 
 	// validate the movie record
 	v := validator.New()
