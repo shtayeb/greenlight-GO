b7c98b9a2055f1af4ffdbc4fcc0e4ca438469ebc
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-11-19 11:37:55 +0430

    global rate limiting

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -55,3 +55,9 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
+
+func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
+	message := "rate limit exceeded"
+
+	app.errorResponse(w, r, http.StatusTooManyRequests, message)
+}
