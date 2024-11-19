3f5f8e1d7874fb468b9b7b18ed596ba09882449e
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-11-19 14:15:00 +0430

    creating a user

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -19,6 +19,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
+	// users
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
 	return app.recoverPanic(
 		app.rateLimit(router),
 	)
