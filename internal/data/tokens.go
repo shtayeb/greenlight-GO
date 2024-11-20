78acf12c12c7da31ed68f35bfe50f17852975a01
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-11-20 12:24:09 +0430

    generating auth token

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -12,13 +12,14 @@ import (
 )
 
 const ScopeActivation = "activation"
+const ScopeAuthentication = "authentication"
 
 type Token struct {
-	Plaintext string
-	Hash      []byte
-	UserID    int64
-	Expiry    time.Time
-	Scope     string
+	Plaintext string    `json:"token"`
+	Hash      []byte    `json:"-"`
+	UserID    int64     `json:"-"`
+	Expiry    time.Time `json:"expiry"`
+	Scope     string    `json:"-"`
 }
 
 func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
