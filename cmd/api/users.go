4c3e238692c81461e5c73f76967054a5ff62e609
Shahryar Tayeb <shahryar.dev@gmail.com>
2024-11-21 14:20:12 +0430

    granting permissions

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -53,6 +53,13 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	// Add permssion to the user
+	err = app.models.Permissions.AddForUser(user.ID, "movies:read")
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	// After the user record has been created in the database, generate a new activation token for the user
 	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
 	if err != nil {
--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 // Define a Permissions slice, which we will use to will hold the permission codes (like "movies:read" and "movies:write") for a single user.
@@ -58,4 +60,17 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	}
 
 	return permissions, nil
+}
+
+func (m PermissionModel) AddForUser(userID int64, code ...string) error {
+	query := `
+		INSERT INTO users_permissions
+		SELECT $1,permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, pq.Array(code))
+
+	return err
 }
